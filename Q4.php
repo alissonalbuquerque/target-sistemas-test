a07cd73e8d7b4e22cc97a422d214202c5098abd4
Alisson Albuquerque <alissonalbuquerque.dev@gmail.com>
2025-01-14 14:25:24 -0300

    add question 04

--- Q4.php
+++ Q4.php
@@ -7,3 +7,19 @@ $month_faturament = [
     'ES' => 27165.48,
     'OTHERS' => 19849.53
 ];
+
+$total = 
+    array_reduce($month_faturament, function($carry, $item) {
+        $carry = $carry + $item;
+        return $carry;
+    }, 0.0);
+
+$month_percentage = [];
+foreach($month_faturament as $key => $value) {
+    $month_percentage[$key] = ($value / $total) * 100.0;
+    $month_percentage[$key] = number_format($month_percentage[$key], 2, ',', '.');
+}
+
+foreach($month_percentage as $key => $value) {
+    echo "$key -> $value %" . "\n";
+}
